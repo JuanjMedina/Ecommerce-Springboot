a2e567b32784d808ddd432e6c698b7a0c98685a5
Juan Medina <jmedinaguerrero847@gmail.com>
2025-05-17 22:27:16 -0500

    update ID generation strategy for Order and User models to use IDENTITY

--- src/main/java/com/dev/ecomercespringboot/model/Order.java
+++ src/main/java/com/dev/ecomercespringboot/model/Order.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @Table(name = "Orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String number;
     private Date date_created;
--- src/main/java/com/dev/ecomercespringboot/model/User.java
+++ src/main/java/com/dev/ecomercespringboot/model/User.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Table(name = "Users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String username;
