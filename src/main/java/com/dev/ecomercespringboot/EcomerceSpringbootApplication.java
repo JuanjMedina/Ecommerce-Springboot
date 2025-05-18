89e15fbc3db381d9a485386f9eb72985f25809bc
Juan Medina <jmedinaguerrero847@gmail.com>
2025-05-17 22:11:57 -0500

    configure application properties for MySQL datasource and enable JPA settings

--- src/main/java/com/dev/ecomercespringboot/EcomerceSpringbootApplication.java
+++ src/main/java/com/dev/ecomercespringboot/EcomerceSpringbootApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class EcomerceSpringbootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EcomerceSpringbootApplication.class, args);
     }
